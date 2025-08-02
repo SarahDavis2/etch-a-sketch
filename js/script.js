c223e3b5a7f23c40a922ecd53f15e11fb09a4fcb
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-08-02 16:41:41 -0500

    create grid of divs through js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const NUM_OF_SQUARES = 16 * 16;
+
+// Create 16x16 grid of square divs
+const gridContainer = document.querySelector(".grid-container");
+for (let count = 0; count < NUM_OF_SQUARES; count++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+
+    // Test
+    square.textContent = "test";
+}
